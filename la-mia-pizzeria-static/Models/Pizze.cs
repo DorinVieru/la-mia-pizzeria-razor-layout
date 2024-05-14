e49031fd9b73964301f5f12dc1903dbd7fccc424
DorinVieru <dorino.vieru@gmail.com>
2024-05-14 12:31:08 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Models/Pizze.cs
@@ -0,0 +1,20 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizze
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Img { get; set; }
+        public double Price { get; set; }
+
+        public Pizze() { }
+
+        public Pizze(string name, string description, string img, double price)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Img = img;
+            this.Price = price;
+        }
+    }
+}
